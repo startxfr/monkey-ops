daa40e915ad74f6e4cdfea8f3d89820217c4bfe2
xis15930 <julian.fernandez@servexternos.isban.es>
2016-05-25 17:39:34 +0200

    Reading service account token

--- go/monkey-ops.go
+++ go/monkey-ops.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"io/ioutil"
 	
 	"github.com/spf13/viper"
 	flag "github.com/spf13/pflag"
@@ -12,7 +13,6 @@ func main() {
 	
 	flag.String("REGION_URL", "https://api.boae.paas.gsnetcloud.corp:8443", "Region URL")
 	flag.String("PROJECT_NAME", "devstack-dev", "Project to get crazy")
-	flag.String("TOKEN", "RXLVr3T4ft11pneIQzFNpqtdaIl0krl8R4CzcW8uUxc", "Service Account Token")
 	flag.Float64("INTERVAL", 20, "interval time in seconds")
 	flag.Float64("TOTAL_TIME", 0, "total time of chaos monkey in seconds")
 	flag.String("MODE", "background", "Execution mode: background or rest")
@@ -21,14 +21,12 @@ func main() {
 	//Binding flags and env vars
 	viper.BindPFlag( "REGION_URL", flag.Lookup("REGION_URL") )
 	viper.BindPFlag( "PROJECT_NAME", flag.Lookup("PROJECT_NAME") )
-	viper.BindPFlag( "TOKEN", flag.Lookup("TOKEN") )
 	viper.BindPFlag( "INTERVAL", flag.Lookup("INTERVAL") )
 	viper.BindPFlag( "TOTAL_TIME", flag.Lookup("TOTAL_TIME") )
 	viper.BindPFlag( "MODE", flag.Lookup("MODE") )
 	
 	viper.BindEnv("REGION_URL")
 	viper.BindEnv("PROJECT_NAME")
-	viper.BindEnv("TOKEN")
 	viper.BindEnv("INTERVAL")
 	viper.BindEnv("TOTAL_TIME")
 	viper.BindEnv("MODE")
@@ -38,10 +36,17 @@ func main() {
 	//set configuration
 	regionUrl := viper.GetString("REGION_URL")
 	project := viper.GetString("PROJECT_NAME")
-	token := viper.GetString("TOKEN")
 	interval := viper.GetFloat64("INTERVAL")
 	totalTime := viper.GetFloat64("TOTAL_TIME")
 	mode := viper.GetString("MODE")
+	
+	// read the service account secret token file at once
+    tokenBytes, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/token")
+    if err != nil {
+        panic(err)
+    }
+
+    token := string(tokenBytes[:])
 	
 	chaosInput:= ChaosInput{
 		Url: regionUrl,
